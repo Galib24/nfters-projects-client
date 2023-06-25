eec47220494412f12adb180c0f0a945a60944526
SQLAdmin <abuyeahia24@gmail.com>
2023-06-25 06:50:45 +0600

    fix ui design of footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FaFacebookSquare, FaTwitterSquare, FaLinkedin } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <>
             <div>
                 <footer className="footer p-10 bg-white text-black">
-                
+
                     <div>
                         <span className="footer-title">Social</span>
                         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
@@ -14,47 +15,69 @@ const Footer = () => {
                                 <h1 className='text-3xl font-bold uppercase'>nfters</h1>
                                 <p className='my-7'>The world's first largest digital marketplace for crypto collections and non-fungible tokens (NFT's) Buys, sell and discover exclusive items </p>
                                 <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3  '>
-                                <div style={{width: '50px'}} className='btn btn-ghost rounded-full text-white  bg-blue-400 '>
-                                <FaFacebookSquare className='rounded-full w-5 text-2xl'></FaFacebookSquare>
-                                </div>
-                                <div style={{width: '50px'}} className='btn btn-ghost rounded-full text-white  bg-sky-400 '>
-                                <FaTwitterSquare className='rounded-full  w-5 text-2xl'></FaTwitterSquare>
-                                </div>
-                                <div style={{width: '50px'}} className='btn btn-ghost rounded-full text-white  bg-sky-600 '>
-                                <FaLinkedin className='rounded-full text-2xl'></FaLinkedin>
-                                </div>
+                                    <div style={{ width: '50px' }} className='btn btn-ghost rounded-full text-white  bg-blue-400 '>
+                                        <FaFacebookSquare className='rounded-full w-5 text-2xl'></FaFacebookSquare>
+                                    </div>
+                                    <div style={{ width: '50px' }} className='btn btn-ghost rounded-full text-white  bg-sky-400 '>
+                                        <FaTwitterSquare className='rounded-full  w-5 text-2xl'></FaTwitterSquare>
+                                    </div>
+                                    <div style={{ width: '50px' }} className='btn btn-ghost rounded-full text-white  bg-sky-600 '>
+                                        <FaLinkedin className='rounded-full text-2xl'></FaLinkedin>
+                                    </div>
                                 </div>
                             </div>
 
                             <div className='ml-5'>
-                                <h1 className='text-3xl'>Market Place</h1>
-                                <ul className='mt-7'>
-                                    <li>All NFTs</li>
-                                    <li>New</li>
-                                    <li>Art</li>
-                                    <li>Sports</li>
-                                    <li>Utility</li>
-                                    <li>Music</li>
-                                    <li>Domain Name</li>
+                                <h1 className='text-3xl font-semibold'>Market Place</h1>
+                                <ul className='mt-7 font-semibold'>
+                                    <Link>
+                                        <li>All NFTs</li>
+                                    </Link>
+                                    <Link>
+                                        <li>New</li>
+                                    </Link>
+                                    <Link>
+                                        <li>Art</li>
+                                    </Link>
+                                    <Link>
+                                        <li>Sports</li>
+                                    </Link>
+                                    <Link>
+                                        <li>Utility</li>
+                                    </Link>
+                                    <Link>
+                                        <li>Music</li>
+                                    </Link>
+                                    <Link>
+                                        <li>Domain Name</li>
+                                    </Link>
                                 </ul>
                             </div>
 
                             <div>
-                                <h1 className='text-3xl'>My Account</h1>
-                                <ul className='mt-7'>
-                                    <li>Profile</li>
-                                    <li>Favorite</li>
-                                    <li>My Collections</li>
-                                    <li>Settings</li>
+                                <h1 className='text-3xl font-semibold'>My Account</h1>
+                                <ul className='mt-7 font-semibold'>
+                                    <Link>
+                                        <li>Profile</li>
+                                    </Link>
+                                    <Link>
+                                        <li>Favorite</li>
+                                    </Link>
+                                    <Link>
+                                        <li>My Collections</li>
+                                    </Link>
+                                    <Link>
+                                        <li>Settings</li>
+                                    </Link>
                                 </ul>
                             </div>
 
-                            <div>
-                                <h1 className='text-3xl'> Stay The Loop</h1>
+                            <div className='w-full'>
+                                <h1 className='text-3xl font-semibold'> Stay The Loop</h1>
                                 <p className='my-7'>Join Our mailing list to stay the loop with our newest feature releases, NFT drops and tips for navigating NFTs</p>
                                 <div className='flex relative'>
-                                <input className='rounded-full w-full  border-y-4 px-20 py-2' placeholder='Email Address' type="text" name="" id=""  />
-                                <button className='btn-primary rounded-3xl absolute right-0 mt-1 py-2 px-3  text-white font-semibold '>Subscribe Now</button>
+                                    <input className='rounded-full w-full  border-y-4 px-20 py-2' placeholder='Email Address' type="text" name="" id="" />
+                                    <button className='btn-primary rounded-3xl absolute right-0 mt-1 py-2 px-3   text-white font-semibold '>Subscribe Now</button>
                                 </div>
                             </div>
 
